d44bf9914aa5933eb09a5af91b2a212513562502
Thakur-Tanishq <thakurtanishq489@gmail.com>
2026-03-16 13:08:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,152 +1,106 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDykC9suuhhL8Krhui2t6npS-LJy_k_YFc",
-  authDomain: "chat-e7b28.firebaseapp.com",
-  databaseURL: "https://chat-e7b28-default-rtdb.firebaseio.com",
-  projectId: "chat-e7b28",
-  storageBucket: "chat-e7b28.firebasestorage.app",
-  messagingSenderId: "276902406806",
-  appId: "1:276902406806:web:1b7bab41defb69e3a770b7"
+
+apiKey:"AIzaSyDykC9suuhhL8Krhui2t6npS-LJy_k_YFc",
+authDomain:"chat-e7b28.firebaseapp.com",
+databaseURL:"https://chat-e7b28-default-rtdb.firebaseio.com",
+projectId:"chat-e7b28"
+
 };
 
 firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
 
-let username="";
+const db=firebase.database();
 
-const chat=document.getElementById("chat");
-const typingDiv=document.getElementById("typing");
+let username=""
+let deviceId=localStorage.getItem("deviceId")
 
+if(!deviceId){
 
-/* LOGIN */
+deviceId=Math.random().toString(36)
+localStorage.setItem("deviceId",deviceId)
+
+}
 
 function login(){
 
-const pass=document.getElementById("password").value;
-username=document.getElementById("username").value;
+username=document.getElementById("username").value
+const pass=document.getElementById("password").value
 
-if(pass==="aditi777" && username!=""){
+if(pass!=="aditi777" && pass!=="admin777"){
 
-document.getElementById("lockPage").style.display="none";
-document.getElementById("chatPage").style.display="flex";
-
-db.ref("online/"+username).set(true);
-
-startChat();
-
-}else{
-
-document.getElementById("error").innerText="Wrong password or username";
+document.getElementById("error").innerText="Wrong password"
+return
 
 }
 
+document.getElementById("login").style.display="none"
+document.getElementById("chatPage").style.display="flex"
+
+db.ref("online/"+deviceId).set(username)
+
+window.onbeforeunload=()=>{
+
+db.ref("online/"+deviceId).remove()
+
 }
 
+loadMessages()
 
-/* ONLINE USERS */
+}
+
 
 db.ref("online").on("value",(snap)=>{
 
-document.getElementById("online").innerText="Online: "+snap.numChildren();
+document.getElementById("online").innerText="Online: "+snap.numChildren()
 
-});
+})
 
-
-/* CHAT */
-
-function startChat(){
-
-db.ref("messages").on("child_added",(snap)=>{
-
-const data=snap.val();
-
-const div=document.createElement("div");
-
-div.classList.add("message");
-
-if(data.user===username){
-
-div.classList.add("me");
-
-}else{
-
-div.classList.add("other");
-
-db.ref("messages/"+snap.key+"/seen").set(true);
-
-}
-
-let tick="";
-
-if(data.user===username){
-
-tick=data.seen ? "✓✓" : "✓";
-
-}
-
-div.innerHTML=data.user+": "+data.text+" <span class='tick'>"+tick+"</span>";
-
-chat.appendChild(div);
-
-chat.scrollTop=chat.scrollHeight;
-
-});
-
-}
-
-
-/* SEND */
 
 function sendMessage(){
 
-const text=document.getElementById("message").value;
+const text=document.getElementById("message").value
 
-if(text==="") return;
+if(text==="") return
 
-const id=Date.now();
+const time=new Date().toLocaleTimeString()
 
-db.ref("messages/"+id).set({
+db.ref("messages").push({
 
 user:username,
 text:text,
-seen:false
+time:time
 
-});
+})
 
-document.getElementById("message").value="";
-
-}
-
-
-/* TYPING */
-
-function typing(){
-
-db.ref("typing").set(username+" typing...");
-
-setTimeout(()=>{
-
-db.ref("typing").set("");
-
-},1000);
+document.getElementById("message").value=""
 
 }
 
-db.ref("typing").on("value",(snap)=>{
 
-typingDiv.innerText=snap.val();
+function loadMessages(){
 
-});
+db.ref("messages").on("child_added",(snap)=>{
 
+const data=snap.val()
 
-/* CLEAR */
+const div=document.createElement("div")
 
-function clearChat(){
+div.className="message"
 
-if(confirm("Delete all messages?")){
+if(data.user===username){
 
-db.ref("messages").remove();
-chat.innerHTML="";
+div.classList.add("me")
+
+}else{
+
+div.classList.add("other")
 
 }
+
+div.innerHTML=data.user+": "+data.text+" <span class='time'>"+data.time+"</span>"
+
+document.getElementById("chat").appendChild(div)
+
+})
 
 }
